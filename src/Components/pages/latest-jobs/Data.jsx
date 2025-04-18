e86ce0c1c66b46c4a8bce2753256d7044c124c61
rahulkumar9918 <rahulmobile0011@gmail.com>
2025-04-17 21:47:43 -0700

    Add files via upload

--- src/Components/pages/latest-jobs/Data.jsx
+++ src/Components/pages/latest-jobs/Data.jsx
@@ -1,5 +1,85 @@
 const jobList = [
   {
+    id: 4,
+    title: "Allahabad University Teaching Recruitment Online Form 2025 Last Date:02/05/2025",
+    postName:
+      "NAllahabad University Professor, Associate Professor & Assistant Professor Recruitment 2025",
+    Eligibility:
+      "Rajasthan CET 10+2 Level Exam Passed 10+2 Intermediate Exam in Any Recognized Board in India More Eligibility Details Must Read the Notification",
+    vacancy: "200",
+    importantDates: [
+      { label: "Application Begin", value: "17/04/2025" },
+      { label: "Last Date to Apply", value: "10/05/2025" },
+      { label: "Pay the Exam Last Date", value: "10/05/2025" },
+      { label: "Exam Date", value: "As per Schedule" },
+      { label: "Admit Card Available", value: "Before Exam" },
+    ],
+    howToApply : [
+      "Northern Coalfield Limited NCL Technician Latest Recruitment 2025 for the Post of Technician Post Can Apply Between  17/04/2025 to 10/05/2025.",
+      "Candidate Read the Notification Before Apply the Vacancies Application Form in Govt. Jobs of NCL Technician NCL CIL Career Recruitment Online Form 2025.",
+      "Kindly Check and Collect the All Document - Eligibility ID Proof Address Details Basic Details.",
+      "Kindly Ready Scan Document Related to Recruitment Form - Photo Sign ID Proof Etc.",
+      "Before Apply Online / Submit Application Form Must Check the Preview and All Column Carefully.",
+      "Take A Print Out of Final Submitted Form.",
+    ],
+    applicationFee: [
+      { label: "General/OBC/EWS", value: "1180/-" },
+      { label: "SC/ST/PH", value: "0" },
+      {
+        label:
+          "Pay the Exam Fee Through Debit Card / Credit Card / Net Banking Only",
+      },
+    ],
+    officialLinks: [
+      { label: "Apply Online", to: "https://cdn.digialm.com//EForms/configuredHtml/1258/92843/Index.html" },
+      {
+        label: "Download Notification",
+        to: "https://doc.sarkariresults.org.in/NCL%20Technician%202025.pdf",
+      },
+      { label: "Official Website", to: "https://www.nclcil.in/" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Northern Coalfield NCL Technician Online Form 2025 Last Date:10/05/2025",
+    postName:
+      "Northern Coalfield Limited NCL CIL Technician Recruitment 2025 Apply Online for 200 Post",
+    Eligibility:
+      "Class 10th Matric Exam Passed from Any Recognized Board in India/ITI 2 Year Course Certificate in Related Trade Branch 1 Year Apprentice Training Certificate",
+    vacancy: "200",
+    importantDates: [
+      { label: "Application Begin", value: "17/04/2025" },
+      { label: "Last Date to Apply", value: "10/05/2025" },
+      { label: "Pay the Exam Last Date", value: "10/05/2025" },
+      { label: "Exam Date", value: "As per Schedule" },
+      { label: "Admit Card Available", value: "Before Exam" },
+    ],
+    howToApply : [
+      "Northern Coalfield Limited NCL Technician Latest Recruitment 2025 for the Post of Technician Post Can Apply Between  17/04/2025 to 10/05/2025.",
+      "Candidate Read the Notification Before Apply the Vacancies Application Form in Govt. Jobs of NCL Technician NCL CIL Career Recruitment Online Form 2025.",
+      "Kindly Check and Collect the All Document - Eligibility ID Proof Address Details Basic Details.",
+      "Kindly Ready Scan Document Related to Recruitment Form - Photo Sign ID Proof Etc.",
+      "Before Apply Online / Submit Application Form Must Check the Preview and All Column Carefully.",
+      "Take A Print Out of Final Submitted Form.",
+    ],
+    applicationFee: [
+      { label: "General/OBC/EWS", value: "1180/-" },
+      { label: "SC/ST/PH", value: "0" },
+      {
+        label:
+          "Pay the Exam Fee Through Debit Card / Credit Card / Net Banking Only",
+      },
+    ],
+    officialLinks: [
+      { label: "Apply Online", to: "https://cdn.digialm.com//EForms/configuredHtml/1258/92843/Index.html" },
+      {
+        label: "Download Notification",
+        to: "https://doc.sarkariresults.org.in/NCL%20Technician%202025.pdf",
+      },
+      { label: "Official Website", to: "https://www.nclcil.in/" },
+    ],
+  },
+  {
     id: 2,
     title: "Rajasthan Police Constable Online Form 2025",
     postName:
@@ -32,7 +112,7 @@ const jobList = [
     ],
   },
   {
-    id: 3,
+    id: 1,
     title: "ISRO VSSC Various Post Online Form 2025",
     postName:
       "ISRO Vikram Sarabhai Space Centre VSSC Assistant, Driver, Fireman, Cook Recruitment 2025 Apply Online for 16 Post",
